a905bce3b2e233ddb99c7e4580d472d14c003af9
zfiromsa <mdzm841@gmail.com>
2023-06-01 11:25:35 -0700

    go

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,19 +4,23 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
     if (parent == NULL)
+    {
         return NULL;
-
+    }
     binary_tree_t *new_node = binary_tree_node(parent, value);
     if (new_node == NULL)
-        return NULL;
-
-    if (parent->right != NULL)
     {
-        new_node->right = parent->right;
-        parent->right->parent = new_node;
+        return NULL;
     }
+    else
+    {
+        if (parent->right != NULL)
+        {
+            new_node->right = parent->right;
+            parent->right->parent = new_node;
+        }
 
-    parent->right = new_node;
-
+        parent->right = new_node;
+    }
     return new_node;
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,7 +3,8 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
     if (node == NULL)
+    {
         return 0;
-
+    }
     return (node->left == NULL && node->right == NULL);
 }
