af64e2368d38bc1ea5ca74fadca77bd97d486aef
zfiromsa <mdzm841@gmail.com>
2023-06-01 11:20:13 -0700

    go

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node == NULL)
+        return 0;
+
+    return (node->left == NULL && node->right == NULL);
+}
