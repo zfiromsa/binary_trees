10fe291598f805ccca496c9980641b7a9c0b1906
zfiromsa <mdzm841@gmail.com>
2024-02-26 06:06:16 -0800

    go

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,8 +4,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
     if (node == NULL)
         return (0);
-    if (node->left && node->right)
-        return (1);
-    return(0);
+    return(node->parent == NULL);
 }
 
