6e35f811387a351590e27efdcd863a0bd213eba7
zfiromsa <mdzm841@gmail.com>
2024-02-26 23:50:56 -0800

    go

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,20 +9,20 @@
  * 
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
-
 {
-    binary_tree_t *right_node;
+	binary_tree_t *right_node;
 
-    if (parent == NULL)
-        return (NULL);
-    right_node = binary_tree_node(parent, value);
-    if (right_node == NULL)
-        return (NULL);
-    if (parent->right != NULL)
-    {
-        right_node->right = parent->right;
-        right_node->right->parent = right_node;
-    }
-    parent->right = right_node;
-    return (right_node);
+	if (parent == NULL)
+		return (NULL);
+	right_node = binary_tree_node(parent, value);
+	if (right_node == NULL)
+		return (NULL);
+	if (parent->right != NULL)
+	{
+		right_node->right = parent->right;
+		right_node->right->parent = right_node;
+	}
+	parent->right = right_node;
+	return (right_node);
 }
+
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,13 +3,13 @@
 /**
  * binary_tree_is_leaf - a function that checks if a given node is a leaf.
  *
- * @node: Apoint to the node to check
- * Return: 1 if its leaf otherwise 0
+ * @node: Apoint to the node to check.
+ * Return: 1 if its leaf otherwise 0.
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
-    return(node->parent == NULL);
+	if (node == NULL)
+		return (0);
+	return(node->parent == NULL);
 }
 
