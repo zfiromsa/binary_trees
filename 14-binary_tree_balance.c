4813ed3626a33656ed50fcf3051b7c2c7432e893
zfiromsa <mdzm841@gmail.com>
2024-02-28 11:04:04 -0800

    go

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balances - a fun counts thenodes with at least 1 child in a binary
+ *
+ * @tree: is a pointer to root node of the tree to balances the number of nodes
+ * Return: rnodes, lnodes.
+*/
+int binary_tree_balances(const binary_tree_t *tree)
+{
+	int lnodes, rnodes;
+
+	rnodes = lnodes = 0;
+	if (tree == NULL)
+		return (lnodes);
+	lnodes = binary_tree_balances(tree->left);
+	rnodes = binary_tree_balances(tree->right);
+	return (lnodes - rnodes);
+}
+
