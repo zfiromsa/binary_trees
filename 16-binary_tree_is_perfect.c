11d23dd26a5fea5e5e239a94628db48e178c9b58
zfiromsa <mdzm841@gmail.com>
2024-02-29 22:10:35 -0800

    go

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,29 +1,38 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - a function that measures the height of a node
+ *
+ * @tree: is a pointer to the node to measure the depth.
+ * Return: If tree is NULL 0 or Lheight, Rheight.
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t Lheight, Rheight;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+	Lheight = binary_tree_height(tree->left);
+	Rheight = binary_tree_height(tree->right);
+	if (Lheight >= Rheight)
+		return (Lheight + 1);
+	return (Rheight + 1);
+}
+
+/**
  * binary_tree_is_perfect - a function that measures the height of a node
  *
  * @tree: is a pointer to the node to measure the depth.
- * Return: 0 or j
+ * Return: 0 or 1
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	const binary_tree_t *Rtree, *Ltree;
 	int Lheight, Rheight;
 
 	if (!tree)
 		return (0);
-	Rheight = 0;
-	Lheight = 0;
-	Ltree = tree->left;
-	Rtree = tree->right;
-	while (Rtree)
-	{
-		Rheight++;
-		Rtree = Rtree->right;
-	}
-	if (Lheight == Rheight)
-		return (1);
-	return (0);
+	Rheight = binary_tree_height(tree->left);
+	Lheight = binary_tree_height(tree->right);
+	return (Rheight == Lheight);
 }
 
